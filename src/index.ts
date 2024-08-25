35983c43eec6d6158dfc0b2dbf7b475402eb5b7f
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-25 19:48:52 +0100

    Include utility methods in export

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,15 @@ import {Shape} from "./classes/Shape.js";
 import {Square} from "./classes/Square.js";
 import {Vector} from "./classes/Vector.js";
 import {Vertex} from "./classes/Vertex.js";
+import {arrayDifference} from "./utility/array-utility/arrayDifference.js";
+import {arrayEquals} from "./utility/array-utility/arrayEquals.js";
+import {arrayIncludesElement} from "./utility/array-utility/arrayIncludesElement.js";
+import {chooseRandomArrayElement} from "./utility/array-utility/chooseRandomArrayElement.js";
+import {objectEquals} from "./utility/object-utility/objectEquals.js";
+import {setDifference} from "./utility/set-utility/setDifference.js";
+import {setEquals} from "./utility/set-utility/setEquals.js";
+import {setIncludesElement} from "./utility/set-utility/setIncludesElement.js";
+import {equals} from "./utility/equals.js";
 
 export {
     HyperCube,
@@ -13,5 +22,14 @@ export {
     Shape,
     Square,
     Vector,
-    Vertex
+    Vertex,
+    arrayDifference,
+    arrayEquals,
+    arrayIncludesElement,
+    chooseRandomArrayElement,
+    objectEquals,
+    setDifference,
+    setEquals,
+    setIncludesElement,
+    equals,
 }
