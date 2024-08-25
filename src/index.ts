2ed77443101a082f28675350a79c03d8eec9504a
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-25 13:46:00 +0100

    Export classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import {HyperCube} from "./classes/HyperCube";
+import {HyperRectangle} from "./classes/HyperRectangle";
+import {Matrix} from "./classes/Matrix";
+import {Shape} from "./classes/Shape";
+import {Square} from "./classes/Square";
+import {Vector} from "./classes/Vector";
+import {Vertex} from "./classes/Vertex";
 
+export {
+    HyperCube,
+    HyperRectangle,
+    Matrix,
+    Shape,
+    Square,
+    Vector,
+    Vertex
+}
